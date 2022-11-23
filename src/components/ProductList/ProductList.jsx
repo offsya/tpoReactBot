58d59b41927f439f3fecae6bc2ed76600720ce87
Yura <idblablobla@gmail.com>
2022-11-24 02:41:03 +0300

    gg1

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -134,11 +134,11 @@ const ProductList = () => {
 
     return (
         <div className={'list'} style={{paddingTop: '100px'}}>
-            <Box sx={{ flexGrow: 1}} style={{marginBottom: '10px', background: 'black'}}>
+            <Box sx={{ flexGrow: 0}} style={{marginBottom: '10px'}}>
                 <AppBar position="fixed">
-                    <Toolbar>
+                    <Toolbar className='searchBar'>
 
-                        <Search style={{width: '70%', minWidth: '200px'}}>
+                        <Search style={{width: '100%', minWidth: '200px'}}>
                             <SearchIconWrapper>
 
                             </SearchIconWrapper>
